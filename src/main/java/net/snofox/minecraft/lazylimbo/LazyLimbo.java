9198840858bcb76df45739618d1777345aed7191
Josh Johnson <snofox@snofox.net>
2020-03-02 23:57:51 -0800

    Send once every 30 sec
    Also, fix project capitalization

--- src/main/java/net/snofox/minecraft/lazylimbo/LazyLimbo.java
+++ src/main/java/net/snofox/minecraft/lazylimbo/LazyLimbo.java
@@ -1,5 +1,7 @@
 package net.snofox.minecraft.lazylimbo;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import la.loa.crispy1989.LOALib.Teleports.BungeeCord;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
@@ -9,9 +11,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 public final class LazyLimbo extends JavaPlugin {
-    final private BaseComponent[] message = new ComponentBuilder("Nexus is down; reconnecting you shortly...")
+    final private BaseComponent[] message = new ComponentBuilder("Nexus is down; reconnecting you shortly")
             .color(ChatColor.RED).create();
+    final private Cache<UUID, Boolean> recentSends = CacheBuilder.newBuilder().expireAfterWrite(30, TimeUnit.SECONDS).build();
 
     @Override
     public void onEnable() {
@@ -24,11 +30,14 @@ public final class LazyLimbo extends JavaPlugin {
                 () -> Bukkit.getOnlinePlayers().forEach(player -> player.spigot().sendMessage(ChatMessageType.ACTION_BAR, message)),
                 20, 20);
         getServer().getScheduler().scheduleSyncRepeatingTask(this,
-                () -> Bukkit.getOnlinePlayers().stream().filter((player) -> !player.isOp()).findFirst().ifPresent(this::sendToNexus),
+                () -> Bukkit.getOnlinePlayers().stream().filter(
+                        (player) -> !player.isOp() && recentSends.getIfPresent(player.getUniqueId()) == null)
+                        .findFirst().ifPresent(this::sendToNexus),
                 20, 20);
     }
 
     private void sendToNexus(final Player player) {
+        recentSends.put(player.getUniqueId(), true);
         BungeeCord.movePlayerToServer(player, "nexus");
     }
 }
